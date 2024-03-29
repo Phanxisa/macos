0c623f0e187d23941ae97a79a799a59ad65a5f74
Phanxisa <165343351+Phanxisa@users.noreply.github.com>
2024-03-29 10:37:10 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #Downloads
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
+curl -s -o login.sh -L "https://raw.githubusercontent.com/Phanxisa/macos/main/start.sh"
 #disable spotlight indexing
 sudo mdutil -i off -a
 #Create new account
